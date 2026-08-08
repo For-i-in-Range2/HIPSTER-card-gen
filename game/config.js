73cbcf097f77a8beb125643cd6dd9d9dfe7af8cd
spy <144661897+For-i-in-Range2@users.noreply.github.com>
2026-08-08 11:57:53 +0200

    blabla

--- game/config.js
+++ game/config.js
@@ -8,7 +8,7 @@ function currentRedirectUri() {
 }
 
 export const CONFIG = {
-  SPOTIFY_CLIENT_ID: "177da32124814750a992ddea6266fb12",
+  SPOTIFY_CLIENT_ID: "8eebf9ac4060443e9cd1d740c030b9a1",
   REDIRECT_URI: currentRedirectUri(),
   CARDS_TO_WIN: 10,
   DEMO_MODE: false,
